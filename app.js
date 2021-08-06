84aacf948239a0e0c2a21bb4fef738c871455039
Ryan Pena <ryanspena@gmail.com>
2021-08-06 16:04:39 -0500

    questions

--- app.js
+++ app.js
@@ -4,89 +4,104 @@ const pageGenerator = require('./src/pageGenerator')
 const inquirer = require('inquirer')
 const Choices = require('inquirer/lib/objects/choices')
 
-const questionMGR = [
- {
-  type: 'input',
-  name: 'managersName',
-  message: 'who is your manager?',
-  validate: nameInput => {
-   if (nameInput) {
-    return true;
-   } else {
-    console.log('enter your managers name');
-    return false;
+const questionMGR = () => {
+ return inquirer.prompt([
+  {
+   type: 'input',
+   name: 'managerName',
+   message: 'Who is your Team Manager(TM)?',
+   validate: nameInput => {
+    if (nameInput) {
+     return true;
+    } else {
+     console.log('enter your managers name');
+     return false;
+    }
    }
-  }
- },
- {type: 'input',
- name: 'managerId',
- message: 'What is your managers ID?'
-},
-{
- type: 'input',
- name: 'managerEmail',
- message: 'What is your managers email?'
-},
-{
- type: 'input',
- name: 'managerId',
- message: 'What is your managers number?'
- },
-]
+  },
+  {
+   type: 'input',
+   name: 'managerId',
+   message: 'What is your managers ID?'
+  },
+  {
+   type: 'input',
+   name: 'managerEmail',
+   message: 'What is your managers email?'
+  },
+  {
+   type: 'input',
+   name: 'managerId',
+   message: 'What is your managers number?'
+  },
+ ])
+}
 
-const questionENG = [
- {
-  type: 'input',
-  name: 'engineerName',
-  message: 'What is the engineers name?',
- },
- {
-  type: 'input',
-  name: 'engineerID',
-  message: 'what is their ID?',
- },
- {
-  type: 'input',
-  name: 'engineerEmal',
-  message: "What is their Email?",
-},
-{
-  type: 'input',
-  name: 'engineerGithub',
-  message: "What is their Github username?",
-},
-]
-
-const questionINT = [
- {
-  type: 'input',
-  name: 'internName',
-  message: 'what is the interns name?'
- },
- {
-  type: 'input',
-  name: 'internID',
-  message: 'what is their ID'
- },
- {
-  type: 'input',
-  name: 'internEmail',
-  message: 'what is their email'
- },
- {
-  type: 'input',
-  name: 'internGitHub',
-  message: 'What is the name of the school they are attending'
- },
-]
+const questionENG = () => {
+ return inquirer.prompt(
+  [
+   {
+    type: 'input',
+    name: 'engineerName',
+    message: 'What is the engineers name?',
+   },
+   {
+    type: 'input',
+    name: 'engineerID',
+    message: 'what is their ID?',
+   },
+   {
+    type: 'input',
+    name: 'engineerEmal',
+    message: "What is their Email?",
+   },
+   {
+    type: 'input',
+    name: 'engineerGithub',
+    message: "What is their Github username?",
+   },
+  ]
+ ).then(engineerAnswer => {
+  console.log(engineerAnswers)
+  return menu()
+ })
+}
+const questionINT = () => {
+ return inquirer.prompt(
+  [
+   {
+    type: 'input',
+    name: 'internName',
+    message: 'what is the interns name?'
+   },
+   {
+    type: 'input',
+    name: 'internID',
+    message: 'what is their ID'
+   },
+   {
+    type: 'input',
+    name: 'internEmail',
+    message: 'what is their email'
+   },
+   {
+    type: 'input',
+    name: 'internGitHub',
+    message: 'What is the name of the school they are attending'
+   },
+  ]
+ ).then(internAnswers => {
+  console.log(internAnswers)
+  return menu()
+ })
+}
 
 function writeToFile(fileName, data) {
  return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 function startup() {
- inquirer.prompt(questions)
-  .then(response => {
-  writeToFile('index.html', pageGenerator(responses))
- })
+ questionsMGR()
+  .then(menu)
+ .then(responses => { console.log(responses) })
 }
 startup()
